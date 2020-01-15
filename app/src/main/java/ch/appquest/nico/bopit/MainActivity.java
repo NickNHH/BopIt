9f25913866da55f8e841ec1b123b776ea7572a82
Nico Tuscano <nico.tuscano@adnovum.ch>
2020-01-15 13:42:07 +0100

    Change main activity layout

--- app/src/main/java/ch/appquest/nico/bopit/MainActivity.java
+++ app/src/main/java/ch/appquest/nico/bopit/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton settingsBtn = findViewById(R.id.settingsBtn);
+        settingsBtn.setImageResource(R.drawable.icon_settings);
+
+        ImageButton tutorialBtn = findViewById(R.id.tutorialBtn);
+        tutorialBtn.setImageResource(R.drawable.icon_help);
     }
 
     //Called when singleplayer button is pressed
