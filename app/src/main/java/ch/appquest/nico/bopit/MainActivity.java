72d381aa4022c7e2d2e278752e414a108e164fe0
Nico Tuscano <nico.tuscano@adnovum.ch>
2020-01-15 11:26:02 +0100

    Add activities for other modes

--- app/src/main/java/ch/appquest/nico/bopit/MainActivity.java
+++ app/src/main/java/ch/appquest/nico/bopit/MainActivity.java
@@ -2,7 +2,10 @@ package ch.appquest.nico.bopit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Called when singleplayer button is pressed
+    public void playSingleplayer(View view) {
+        Intent intent = new Intent(this, SingleplayerActivity.class);
+        startActivity(intent);
+    }
+
+    //Called when multiplayer button is pressed
+    public void playMultiplayer(View view) {
+        Intent intent = new Intent(this, MultiplayerActivity.class);
+        startActivity(intent);
+    }
+
+    //Called when tutorial button is pressed
+    public void showTutorial(View view) {
+        Intent intent = new Intent(this, TutorialActivity.class);
+        startActivity(intent);
+    }
+
+    //Called when settings button is pressed
+    public void showSettings(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/ch/appquest/nico/bopit/SingleplayerActivity.java
@@ -0,0 +1,14 @@
+package ch.appquest.nico.bopit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleplayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singleplayer);
+    }
+}
