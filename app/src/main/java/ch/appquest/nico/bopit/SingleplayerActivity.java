5d0835e6ba4fe72ed82e7c5d314d4c7afd420b09
Nico Tuscano <nico.tuscano@adnovum.ch>
2020-01-15 15:34:58 +0100

    Add layout to singleplayer activity

--- app/src/main/java/ch/appquest/nico/bopit/SingleplayerActivity.java
+++ app/src/main/java/ch/appquest/nico/bopit/SingleplayerActivity.java
@@ -3,6 +3,7 @@ package ch.appquest.nico.bopit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SingleplayerActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class SingleplayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singleplayer);
+    }
+
+    public void bopItClicked(View view) {
     }
 }
