22f4d7547042eeeeb77383fed6586fb39519d113
Nico Tuscano <nico.tuscano@adnovum.ch>
2020-01-16 09:27:57 +0100

    Add gameloop logic

--- app/src/main/java/ch/appquest/nico/bopit/Game.java
+++ app/src/main/java/ch/appquest/nico/bopit/Game.java
@@ -4,32 +4,41 @@ import java.util.Random;
 
 class Game {
     private Commands commands = new Commands();
-
-    void gameLoop() {
-
-    }
+    private Command currentCommand;
+    private int score = 0;
+    private int gameDelay = 5000;
 
     Command chooseCommand() {
         commands.setCommands();
         Random random = new Random();
         int number = random.nextInt(commands.getCommands().size());
+        currentCommand = commands.getCommands().get(number);
 
-        return commands.getCommands().get(number);
+        return currentCommand;
     }
 
     void increaseSpeed() {
-
+        //TODO: Minimum speed
+        gameDelay -= 200;
     }
 
-    void checkIfValid() {
+    int getGameSpeed() {
+        return gameDelay;
+    }
 
+    boolean checkIfValid() {
+        return true;
     }
 
     void addScore() {
+        score += currentCommand.getPoints() ;
+    }
 
+    int getScore() {
+        return score;
     }
 
     void endGame() {
-
+        //TODO: Switch activity
     }
 }
--- app/src/main/java/ch/appquest/nico/bopit/SingleplayerActivity.java
+++ app/src/main/java/ch/appquest/nico/bopit/SingleplayerActivity.java
@@ -3,9 +3,14 @@ package ch.appquest.nico.bopit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
 
 public class SingleplayerActivity extends AppCompatActivity {
+    TextView commandView;
+    TextView scoreView;
+
     Commands commands;
     Game game = new Game();
 
@@ -14,12 +19,43 @@ public class SingleplayerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singleplayer);
 
+        commandView = findViewById(R.id.commandText);
+        scoreView = findViewById(R.id.score);
+
         commands = new Commands();
         commands.setCommands();
+
+        startGameLoop();
     }
 
     public void bopItClicked(View view) {
         Command command = game.chooseCommand();
         System.out.println(command.getName());
+    }
+
+    private void startGameLoop() {
+        boolean inProgress = true;
+        final boolean[] isValid = {true};
+        while (inProgress) {
+            Command command = game.chooseCommand();
+            commandView.setText(command.getName());
+
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    // Actions to do after given seconds
+                    isValid[0] = game.checkIfValid();
+                }
+            }, game.getGameSpeed());
+
+            if (isValid[0]) {
+                game.addScore();
+                scoreView.setText(game.getScore());
+                game.increaseSpeed();
+            } else {
+                game.endGame();
+                inProgress = false;
+            }
+        }
     }
 }
